6b8c2b6c7f094779f2ac6f7f5108c325503bfb30
BayuSantoso23 <alex74579@gmail.com>
2024-11-02 23:23:08 +0700

    Second Commit

---
+++ MVC/app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php
+
+class About {
+    public function index($name = 'Budi', $job = 'Proggrammer'){
+        echo "Hello My name is $name, my job is $job";
+    }
+    public function page(){
+        echo 'About/page';
+    }
+}
